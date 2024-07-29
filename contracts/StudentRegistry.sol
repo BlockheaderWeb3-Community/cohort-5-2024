4b1b80fd7e8e6f7e4f300469dd7871fe5b398e2d
Pvsaint <vpeter817@gmail.com>
2024-07-29 09:54:28 +0100

    added functions to update and delete

--- contracts/StudentRegistry.sol
+++ contracts/StudentRegistry.sol
@@ -31,15 +31,14 @@ contract StudentRegistry {
         _;
     }
 
+    // student to the students array
     function addStudent(
         address _studentAddr,
         string memory _name,
         uint8 _age
     ) public onlyOwner isNotAddressZero {
-
         require( bytes(_name).length > 0, "Name cannot be blank");
         require( _age >= 18, "This student is under age");
-
         uint256 _studentId = students.length + 1;
         Student memory student = Student({
             studentAddr: _studentAddr,
@@ -49,28 +48,27 @@ contract StudentRegistry {
         });
 
         students.push(student);
+
         // add student to studentsMapping
         studentsMapping[_studentAddr] = student;
     }
 
+    // get student (in the array method)
     function getStudent(uint8 _studentId) public isNotAddressZero view returns (Student memory) {
         return students[_studentId - 1];
     }
 
-
-
+    // get student (in the mapping method)
     function getStudentFromMapping(address _studentAddr)
         public
         isNotAddressZero
         view
-        returns (Student memory)
-    {
+        returns (Student memory) {
         return studentsMapping[_studentAddr];
     }
 
-
-
-    function deleteStudent(address _studentAddr) public onlyOwner  isNotAddressZero{
+    // delete student from the register (in the mapping method)
+    function deleteStudent(address _studentAddr) public onlyOwner  isNotAddressZero {
 
         require(studentsMapping[_studentAddr].studentAddr != address(0), "Student does not exist");
 
@@ -85,5 +83,17 @@ contract StudentRegistry {
 
         studentsMapping[_studentAddr] = student;
 
+    }
+
+     // this function updates the student register (in the array method)
+    function updateStudents(uint8 _index) public view returns(uint8, string memory) {
+        Student memory updateStudent = students[_index];
+        return(updateStudent.age, updateStudent.name);
+    }
+
+    // deletes student from the register (in the array type)
+    function deleteStudent(uint8 _index) public {
+        students[_index] = students[students.length - 1];
+        students.pop();
     }
 }
