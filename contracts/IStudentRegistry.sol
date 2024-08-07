5872048c112b82ab79a215b6c36d0751a71e87e3
limitlxx <elgravicodesh@gmail.com>
2024-08-07 09:29:13 +0100

    student registration and authorization

--- contracts/IStudentRegistry.sol
+++ contracts/IStudentRegistry.sol
@@ -7,11 +7,18 @@ interface IStudentRegistry {
     function addStudent(
         address _studentAddr,
         string memory _name,
-        uint8 _age
-    ) external;
+        uint8 _age,
+        uint256 _price
+    ) external payable;
 
     function getStudent(uint8 _studentID) external view returns (Student memory);
 
     function getStudentFromMapping(address _studentAddr) external view returns (Student memory);
 
+    function confirmAllStudents() external payable;
+
+}
+
+interface IOwnable{
+    function getBalance() external view returns (uint256);
 }
--- contracts/StudentRegistry.sol
+++ contracts/StudentRegistry.sol
@@ -8,26 +8,40 @@ contract StudentRegistry is Ownable {
     //custom erros
     error NameIsEmpty();
     error UnderAge(uint8 age, uint8 expectedAge);
-
-    //custom data type
-   
+    event Log(address indexed sender, uint256 amount);
+    //custom data type   
   
     //dynamic array of students
-    Student[] private students;
+    // Student[] private students;
 
-    mapping(address => Student) public studentsMapping;
+    // Temporary register
+    Student[] public tempRegister;
+    mapping(address => Student) private tempStudentsMapping;
 
+    // Permanent register
+    Student[] public permanentRegister;
+    mapping(address => Student) private permanentStudentsMapping;
+
+    receive() external payable {}
 
     modifier isNotAddressZero() {
         require(msg.sender != address(0), "Invalid Address");
         _;
     }
 
+    // Register Student - Required to pay 1 ether
     function addStudent(
         address _studentAddr,
         string memory _name,
-        uint8 _age
-    ) public  isNotAddressZero {
+        uint8 _age,
+        uint256 _price
+    ) public payable isNotAddressZero {
+        // Ensure the student exists by checking if the studentAddr is not zero address
+        require(tempStudentsMapping[_studentAddr].studentAddr == address(0), "Student already exists.");
+        // Check the value sent with the transaction
+        require(msg.value == fee, "You need to pay exactly 1 ether");
+        require(msg.value == _price, "Amount and Msg.Value doesn't match");
+
         if (bytes(_name).length == 0) {
             revert NameIsEmpty();
         }
@@ -36,17 +50,43 @@ contract StudentRegistry is Ownable {
             revert UnderAge({age: _age, expectedAge: 18});
         }
 
-        uint256 _studentId = students.length + 1;
+        uint256 _studentId = tempRegister.length + 1;
         Student memory student = Student({
             studentAddr: _studentAddr,
             name: _name,
             age: _age,
-            studentId: _studentId
+            studentId: _studentId,
+            hasPaid: true
         });
 
-        students.push(student);
-        // add student to studentsMapping
-        studentsMapping[_studentAddr] = student;
+        // (bool sent,) = address(0).call{value: msg.value}("");
+        // require(sent, "Failed to send Ether");        
+
+        // Store the student in the temporary register and mapping
+        tempRegister.push(student);
+        tempStudentsMapping[_studentAddr] = student;
+    }
+
+
+     // Function to move all students from the temporary register to the permanent register
+    function confirmAllStudents() public payable onlyOwner {
+        // Loop through all students in the tempRegister
+        for (uint256 i = 0; i < tempRegister.length; i++) {
+            Student memory student = tempRegister[i];
+
+            // Add the student to the permanent register and mapping
+            permanentRegister.push(student);
+            permanentStudentsMapping[student.studentAddr] = student;
+
+            // Remove the student from the temporary mapping
+            delete tempStudentsMapping[student.studentAddr];
+        }
+
+        // Clear the temporary register array
+        delete tempRegister;
+
+        // Make withdrawal on confirm
+        return withdraw();
     }
 
     function getStudent(uint8 _studentId)
@@ -55,8 +95,8 @@ contract StudentRegistry is Ownable {
         isNotAddressZero
         returns (Student memory)
     {
-        return students[_studentId - 1];
-    }
+        return permanentRegister[_studentId - 1];
+     }
 
     function getStudentFromMapping(address _studentAddr)
         public
@@ -64,7 +104,7 @@ contract StudentRegistry is Ownable {
         isNotAddressZero
         returns (Student memory)
     {
-        return studentsMapping[_studentAddr];
+        return permanentStudentsMapping[_studentAddr];
     }
 
     function deleteStudent(address _studentAddr)
@@ -73,7 +113,7 @@ contract StudentRegistry is Ownable {
         isNotAddressZero
     {
         require(
-            studentsMapping[_studentAddr].studentAddr != address(0),
+            permanentStudentsMapping[_studentAddr].studentAddr != address(0),
             "Student does not exist"
         );
 
@@ -83,14 +123,31 @@ contract StudentRegistry is Ownable {
             studentAddr: address(0),
             name: "",
             age: 0,
-            studentId: 0
+            studentId: 0,
+            hasPaid: false
         });
 
-        studentsMapping[_studentAddr] = student;
+        permanentStudentsMapping[_studentAddr] = student;
     }
 
 
     function modifyOwner(address _newOwner) public {
         changeOwner(_newOwner);
     }
+
+     function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    // Function to get the number of permanent registered students
+    function getPermanentTotalStudents() public view returns (uint256) {
+        return permanentRegister.length;
+    }
+
+    // Function to get the number of temp registered students
+    function getTemperaryTotalStudents() public view returns (uint256) {
+        return tempRegister.length;
+    }
+
+    
 }
