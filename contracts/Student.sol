3d6d0667c51818ef87e9bfde269a93d40b2e0af4
Abeeujah <abeeujah@gmail.com>
2024-08-09 01:05:37 +0100

    chore: Add NATSpec comments to Student struct

--- contracts/Student.sol
+++ contracts/Student.sol
@@ -1,8 +1,20 @@
- // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
- struct Student {
-        address studentAddr;
-        string name;
-        uint256 studentId;
-        uint8 age;
-    }
+
+/**
+    @title Student Struct
+    @notice This struct represents the details of a student in the registry.
+    @dev The struct is used to store and manage student information.
+    @custom:property studentAddr, The Ethereum address associated with the student.
+    @custom:property name, The name of the student.
+    @custom:property studentId, The unique ID assigned to the student within the registry.
+    @custom:property age, The unique ID assigned to the student within the registry.
+    @custom:property hasPaid, A boolean indicating whether the student has completed the payment required for registration.
+*/
+struct Student {
+    address studentAddr;
+    string name;
+    uint256 studentId;
+    uint8 age;
+    bool hasPaid;
+}
